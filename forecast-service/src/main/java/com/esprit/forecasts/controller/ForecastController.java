de72f9e973270a0c53361cf8276d9719ccd0c946
medaminebenhmida <mohamedamine.benhmida1@esprit.tn>
2023-06-24 08:15:03 +0100

    Commit Weather Forecast microservice

--- forecast-service/src/main/java/com/esprit/forecasts/controller/ForecastController.java
+++ forecast-service/src/main/java/com/esprit/forecasts/controller/ForecastController.java
@@ -21,27 +21,7 @@ public class ForecastController {
 
     ForecastService forecastService;
 
-    @GetMapping
-    public ResponseEntity<List<Forecast>> getAllForecast() {
-        List<Forecast> forecasts = this.forecastService.getAllForecasts();
-        if (forecasts.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } else {
-            return new ResponseEntity<>(forecasts, HttpStatus.OK);
-        }
-    }
-
-    @GetMapping(value ="by/{id}")
-    public ResponseEntity<Forecast> getForecastById(@PathVariable("id") String id) {
-        Optional<Forecast> forecast = this.forecastService.getForecastById(id);
-        if (forecast.isPresent()) {
-            return new ResponseEntity<>(forecast.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @GetMapping("by/{city}")
+    @GetMapping("{city}")
     public Forecast getWeather(@PathVariable String city) {
         Forecast forecast = new Forecast();
         double temperature = generateRandomTemperature();
@@ -55,19 +35,16 @@ public class ForecastController {
     }
 
     private double generateRandomTemperature() {
-        // Generate a random temperature between -10 and 40 degrees Celsius
         Random random = new Random();
         return random.nextDouble() * (40 - (-10)) + (-10);
     }
 
     private int generateRandomHumidity() {
-        // Generate a random humidity percentage between 0 and 100
         Random random = new Random();
         return random.nextInt(101);
     }
 
     private String generateRandomDescription() {
-        // Generate a random weather description from a predefined list
         String[] descriptions = {"Sunny", "Cloudy", "Partly Cloudy", "Rainy", "Thunderstorms"};
         Random random = new Random();
         int index = random.nextInt(descriptions.length);
